45fe2366769906a3ca75b82498cfe902be9e603c
Jim Purvis <quincyb450@gmail.com>
2023-08-03 19:41:10 +1000

    refactor: create lookup hooks

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,30 +1,32 @@
-import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
-import { BsChevronDown } from "react-icons/bs";
-import { Platform } from "../hooks/usePlatforms";
+import {Button, Menu, MenuButton, MenuItem, MenuList} from "@chakra-ui/react";
+import {BsChevronDown} from "react-icons/bs";
+import {Platform} from "../hooks/usePlatforms";
 import usePlatforms from "../hooks/usePlatforms";
+import usePlatform from "../hooks/usePlatform";
 
-interface Props { 
-  onSelectPlatform: (platform: Platform) => void;
-  selectedPlatformId?: number;
+interface Props {
+    onSelectPlatform: (platform: Platform) => void;
+    selectedPlatformId?: number;
 }
 
-const PlatformSelector = ({ onSelectPlatform, selectedPlatformId }: Props) => {
-  const { data, error } = usePlatforms();
+const PlatformSelector = ({onSelectPlatform, selectedPlatformId}: Props) => {
+    const {data, error} = usePlatforms();
 
-  if (error) return null;
+    const selectedPlatform = usePlatform(selectedPlatformId)
 
-  const selectedPlatform = data?.results.find(p => p.id === selectedPlatformId)
-  
-  return (
-    <Menu>
-      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        {selectedPlatform?.id || 'Platforms'}
-      </MenuButton>
-      <MenuList>
-        {data?.results.map(platform => <MenuItem onClick={() => onSelectPlatform(platform)} key={platform.id}>{platform.name}</MenuItem>)}
-      </MenuList>
-    </Menu>
-  );
+    if (error) return null;
+
+    return (
+        <Menu>
+            <MenuButton as={Button} rightIcon={<BsChevronDown/>}>
+                {selectedPlatform?.id || 'Platforms'}
+            </MenuButton>
+            <MenuList>
+                {data?.results.map(platform => <MenuItem onClick={() => onSelectPlatform(platform)}
+                                                         key={platform.id}>{platform.name}</MenuItem>)}
+            </MenuList>
+        </Menu>
+    );
 };
 
 export default PlatformSelector;
