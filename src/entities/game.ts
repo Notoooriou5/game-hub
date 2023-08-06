8b252f3b0eea44df9141ea9cd4522a88e2ae828b
Jim Purvis <quincyb450@gmail.com>
2023-08-06 22:47:05 +1000

    build game attributes

--- src/entities/game.ts
+++ src/entities/game.ts
@@ -1,11 +1,15 @@
 import {Platform} from "./platform";
+import {Genre} from "./genre";
+import {Publisher} from "./publisher";
 
 export interface Game {
     id: number;
     name: string;
     background_image: string;
+    genres: Genre[]
     parent_platforms: { platform: Platform }[];
     metacritic: number;
+    publishers: Publisher[]
     rating_top: number;
     slug: string
     description_raw: string
